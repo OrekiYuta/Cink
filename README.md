43a73a9e850e46a52f67cca588c1233961174576
Orekiyuta <orekiyuta@gmail.com>
2025-11-22 03:11:36 +0800

    feat: add auto commit and deploy script

--- README.md
+++ README.md
@@ -1,28 +1,3 @@
 # Cink
 
 WIP
-
-## Windows auto deploy
-```bat
-@echo off
-color 0a
-
-REM Set the repository path
-set REPO_PATH=<repo_path>
-
-REM Change directory to the repository (also switches drive if needed)
-cd /d "%REPO_PATH%"
-
-REM Clean Hexo cache
-hexo clean
-
-REM Generate static files
-hexo g
-
-REM Deploy to the branch specified in _config.yml (default is master)
-hexo d
-
-REM Wait for 3 seconds before closing
-timeout /t 3 /nobreak
-
-```
---
+++ cmd/cink-auto-commit.bat
@@ -0,0 +1,29 @@
+@echo off
+color 0a
+
+REM Set the repository path
+set REPO_PATH=F:\Code\1.MyGitHub\Cink
+
+REM Change directory to the repository (also switches drive if needed)
+cd /d "%REPO_PATH%"
+
+REM Add all changes to Git
+call git add .
+
+REM Commit with current date and time
+call git commit -m "ci(local): auto commit at %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,8%"
+
+REM Push to current branch
+call git push
+
+REM Clean Hexo cache
+hexo clean
+
+REM Generate static files
+hexo g
+
+REM Deploy to the branch specified in _config.yml (default is master)
+hexo d
+
+REM Wait for 3 seconds before closing
+timeout /t 3 /nobreak
